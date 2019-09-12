09965fa824c6216b713396856a3112c25da2ac85
dimedcusa <55240268+dimedcusa@users.noreply.github.com>
2019-09-12 14:34:37 +0200

    Update python.py

--- python.py
+++ python.py
@@ -9,7 +9,12 @@ string2 = " Hello World"
 print(string + string2)
 var_a_logical = True
 var_b_logical = False
-if a:
+var_c_logical = not False
+if var_a_logical:
     print("True A statemant")
 else:
     print("False A statemant")
+if var_c_logical:
+    print("True C statemant")
+else:
+    print("False C statemant")
